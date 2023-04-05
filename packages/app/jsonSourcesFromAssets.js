bd767ca4c957c0112ff64141fa45c17f0c5268fb
Matteo Pellegrino <matteo.pelle.pellegrino@gmail.com>
2023-04-05 23:15:22 +0200

    ci: Add script to generate sources JSON from assets

---
+++ packages/app/jsonSourcesFromAssets.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const process = require("process");
+const path = require("path");
+
+const args = process.argv.slice(2);
+
+if (args.length != 1) {
+  console.error("Usage: node sourcesJsonFromAssets.js <folder>");
+  process.exit(1);
+}
+
+fs.readdir(`${__dirname}/public/assets/${args[0]}`, (err, files) => {
+  if (err) {
+    console.error(err);
+    process.exit(2);
+  }
+  files.sort;
+
+  const sources = files
+    .filter((f) => !f.startsWith("."))
+    .map((f) => {
+      const name = path.parse(f).name;
+      return {
+        id: name.toLocaleLowerCase().replace(" ", "-"),
+        name,
+        url: `assets/${args[0]}/${f}`,
+      };
+    });
+
+  process.stdout.write(JSON.stringify(sources));
+});
